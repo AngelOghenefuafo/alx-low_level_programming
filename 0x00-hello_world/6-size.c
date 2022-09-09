729f90ff33d6d3f41d198a37d1fdc6ddffc79e31
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-08 17:17:21 -0700

    Printing types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point (Printing variuo types)
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+char r;
+int g;
+long int t;
+long long p;
+float a;
+
+printf("Size of a char: %ka byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of an int: %ka byte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a long int: %ka byte(s)\n", (unsigned long)sizeof(t));
+printf("Size of a long long: %ka byte(s)\n", (unsigned long)sizeof(p));
+printf("Size of a float: %ka byte(s)\n", (unsigned long)sizeof(a));
+return (0);
+}
