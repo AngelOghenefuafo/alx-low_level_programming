ef359281d487024e1249f7e529fd627b2bd5d09d
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-09 16:34:59 -0700

    print all possible combinations of three different digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: prints all possible combination of 2-digit numbers
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int r, u, l, j;
+
+	for (r = 48; r <= 57; r++)
+	{
+		for (u = 48; u <= 57; u++)
+		{
+			for (l = 48; l <= 57; l++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+				if (((l + j) > (r + u) &&  l >= r) || r < l)
+				{
+					putchar(r);
+					putchar(u);
+					putchar(' ');
+					putchar(l);
+					putchar(j);
+
+					if (r + u + l + j == 227 && r == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
