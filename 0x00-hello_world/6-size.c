1d9d39e730c2d2b96fe7db554acaf0c60b5feab7
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-08 17:21:51 -0700

    Printing types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ float a;
 printf("Size of a char: %ka byte(s)\n", (unsigned long)sizeof(r));
 printf("Size of an int: %ka byte(s)\n", (unsigned long)sizeof(g));
 printf("Size of a long int: %ka byte(s)\n", (unsigned long)sizeof(t));
-printf("Size of a long long: %ka byte(s)\n", (unsigned long)sizeof(p));
+printf("Size of a long long int: %ka byte(s)\n", (unsigned long)sizeof(p));
 printf("Size of a float: %ka byte(s)\n", (unsigned long)sizeof(a));
 return (0);
 }
