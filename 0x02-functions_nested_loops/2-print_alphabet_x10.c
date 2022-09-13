00e78d557dfa734165866bf545383b40290bf58d
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-13 15:15:41 -0700

    Print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  *print_alphabet_x10 -> Print loercase 10 times
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int r;
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		for r - 'a'; r <= 'z'; r++)
+		for (r - 'a'; r <= 'z'; r++)
 		{
 			_putchar(r);
 		}
