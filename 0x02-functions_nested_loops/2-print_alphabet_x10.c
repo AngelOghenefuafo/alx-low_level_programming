1c066ab5402a471c6bd9d0a0354dac2ff2b33994
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-13 16:29:03 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,17 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 
+		i++;
+
 		_putchar('\n');
 
-		i++;
+	
 	}
 }
