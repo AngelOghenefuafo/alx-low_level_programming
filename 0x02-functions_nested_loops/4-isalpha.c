726f7d20e79b39b9dc4b98d9d28eb6f73ef8e29a
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-13 16:14:19 -0700

    checks alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,10 +2,11 @@
 
 /**
  * _isalpha - Entry point
- * Description: Checks for alphabet characters
- * @c: the integer value it recieves
- * Return: 1 if true 0 if false
+ * @c: the integer value it receives
+ * Description: checks if alphabet
+ * Return: 1 if true. 0 if false
  */
+
 int _isalpha(int c)
 {
 	if (c >= 'a' && c <= 'z')
