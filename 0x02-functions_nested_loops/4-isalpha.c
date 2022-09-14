0b90eb3652ff391edb641c57af6680a4374cbb4a
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-13 19:21:18 -0700

    check alphabetic characters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,22 +3,11 @@
 /**
  * _isalpha - Entry point
  * @c: the integer value it receives
- * Description: checks if alphabet
+ * Description: checks for alphabet
  * Return: 1 if true. 0 if false
  */
 
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-	return (1);
-	}
-	else if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
