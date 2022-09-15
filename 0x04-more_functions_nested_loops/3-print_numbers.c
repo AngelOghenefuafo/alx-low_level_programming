8ec7658353fee0373b14ac0bbc7e583874000e4e
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-15 14:03:44 -0700

    Print 0-9 with new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers: Print numbers from 0-9
+ *
+ * Return: 0
+ */
+void print_numbers(void);
+{
+	int c;
+
+	for (c >= 0; c < 10; c++)
+		_putchar(c + 48);
+
+	_putchar("\n");
+}
