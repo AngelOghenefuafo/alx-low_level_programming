099fd5ed8b960e21626f78836c1a6beff9cfe542
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-15 14:25:15 -0700

    print numbers excluding 2%4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,9 +8,9 @@ void print_most_numbers(void)
 {
 	int x;
 
-	for (x = 48; x < 58; x++)
+	for (x = 0; x < 10; x++)
 	{
-		if (x == 50 ||  x == 52)
+		if (x == 2 &&  x == 4)
 			continue;
 		else
 			_putchar(x);
