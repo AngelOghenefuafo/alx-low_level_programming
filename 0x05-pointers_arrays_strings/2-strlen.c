2bbd62a7c99563e49619bcc981cf1733e6634cf2
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-19 12:59:04 -0700

    checks for string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - checks string length
+ *@s: string to check
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (length);
+}
