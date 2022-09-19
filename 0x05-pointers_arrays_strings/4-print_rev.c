f8eec1292cd55eb861e2e026a5bc0c864a8c7fd7
AngelOghenefuafo <angeloghenefuafo@gmail.com>
2022-09-19 13:26:32 -0700

    Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - Print string in reverse
+ * @s: String to print
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 9;
+
+	while (s[i])
+		i++;
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
